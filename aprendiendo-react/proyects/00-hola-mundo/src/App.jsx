0079807e09091b3c1b818473be7ff7dab02a7d53
leonardo alva <leeoalva@gmail.com>
2025-07-27 17:28:25 -0300

    pasando funciones por props y viendo botones

--- aprendiendo-react/proyects/00-hola-mundo/src/App.jsx
+++ aprendiendo-react/proyects/00-hola-mundo/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Contenedor from './Contenedor';
 import Nabvar from './Navbar';
 import Footer from './Footer';  
+import TwitterFollowCard from './TwitterFollowCard';
 function App() {
 
 
@@ -9,11 +10,16 @@ function App() {
 <>
 <div className='Contenedor-App'>
 <Nabvar />
-<Contenedor />
+<div className='Div-Contenedor'>
+  <div>
+<TwitterFollowCard userName={'DonareloV'} name={'Leonardo Alva'} isFollowing/>
+<TwitterFollowCard userName={'Midudev'} name={'Miguel Angel Duran'} isFollowing/>
+<TwitterFollowCard userName={'PabloHidalgo'} name={'Pablo Hidalgo'} isFollowing={false}/>
+  </div>
 </div>
 <Footer className='footer'/>
-</>
-  )
-}
+</div>
+</>)}
+
 
 export default App
--- aprendiendo-react/proyects/00-hola-mundo/src/Contenedor.jsx
+++ aprendiendo-react/proyects/00-hola-mundo/src/Contenedor.jsx
@@ -7,16 +7,13 @@ function Contenedor() {
   return (
 <>
 <div className='Div-Contenedor'>
-    <Counter />
+    {/* <Counter />
+    <TwitterFollowCard 
+      userName="midudev" 
+      name="Miguel Angel Durán" 
+      isFollowing={true}/> */}
 
-    <div>      
-    <TwitterFollowCard userName="DonareloV" name="Leonardo Alva" isFollowing/>
-    <TwitterFollowCard userName="midudev" name="Miguel Angel" isFollowing />
-    <TwitterFollowCard userName="elonmusk" name="Elon Musk" isFollowing />
-    <TwitterFollowCard userName="jack" name="Jack Dorsey" />
-    <TwitterFollowCard userName="billgates" name="Bill Gates" />
-    </div>
-
+      
     <Card producto="Titulo1" descripcion="esto es una descripcion" precio="este es el precio"/>
     <Card producto="Titulo2" descripcion="esto es una descripcion" precio="este es el precio"/>
     <Card producto="Titulo3" descripcion="esto es una descripcion" precio="este es el precio"/>
