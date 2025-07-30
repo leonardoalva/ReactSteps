bd75f666c1001cc2f11213242a64d7cbf863199d
leonardo alva <leeoalva@gmail.com>
2025-07-30 11:07:27 -0300

    agregando loader en pagina pricipal

--- aprendiendo-react/proyects/00-hola-mundo/src/Hocs/withLoading.jsx
+++ aprendiendo-react/proyects/00-hola-mundo/src/Hocs/withLoading.jsx
@@ -1,10 +1,18 @@
-export const withLoading = (Component) =>{
-    function ComponentWithLoading (props){
-        console.log('Loading...')
-        return (
-            <Component {...props} />
-        )
+import {PacmanLoader} from "react-spinners";
+
+export const withLoading = (Component) => {
+  function ComponentWithLoading(props) {
+    if (!props.items || props.items.length === 0) {
+      // Si no hay items, mostramos un mensaje de carga
+      // Puedes personalizar este mensaje o componente de carga
+      // Aquí se muestra un simple párrafo como ejemplo
+        return (<PacmanLoader className="Pacman"/>);}
+    
+    
+    return <Component {...props}></Component>// 
     }
 
-    return ComponentWithLoading;
+
+
+return ComponentWithLoading;
 }
