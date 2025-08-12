ea72ca5be6f2edfb25d46fd58f6ae0b844530cae
leonardo alva <leeoalva@gmail.com>
2025-08-12 20:06:50 -0300

    carrito en marcha

--- aprendiendo-react/proyects/00-hola-mundo/src/context/CartProvider.jsx
+++ aprendiendo-react/proyects/00-hola-mundo/src/context/CartProvider.jsx
@@ -1,24 +1,24 @@
 import { CartContext } from "./CartContext";
-import { useState } from 'react'
+import { useState } from "react";
 
-function CartProvider({children}){
-const [cart, setCart] = useState([])
-console.log(cart)
+function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+  console.log(cart)
 
-const agregarAlCarrito = (prod)=> setCart([...cart,prod])
+  const agregarAlCarrito= (prod) => {setCart([...cart, prod])};
+    
 
-const getCant = ()=>{
-    const cantidades = cart.map(prod => prod.count)
-    const cantidad = cantidades.reduce((acc,current)=>acc+current,0)
-    return cantidad
+  const getCant = () => {
+    const cantidades = cart.map((prod) => prod.count);
+    const cantidad = cantidades.reduce((acc, current) => acc + current, 0);
+    return cantidad;
+  };
 
-}   
-
-    return(
-        <CartContext.Provider value={{cart,agregarAlCarrito,getCant}}>
-            {children}
-        </CartContext.Provider>
-    )
+  return (
+    <CartContext.Provider value={{ cart, agregarAlCarrito, getCant }}>
+      {children}
+    </CartContext.Provider>
+  );
 }
 
 export default CartProvider;
