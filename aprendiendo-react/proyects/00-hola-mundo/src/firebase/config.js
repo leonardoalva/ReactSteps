f70bf9485f41d774c1eb6cf0fbc793b4e4041562
leonardo alva <leeoalva@gmail.com>
2025-08-13 11:17:18 -0300

    agregando dependencias de firebase

---
+++ aprendiendo-react/proyects/00-hola-mundo/src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl6-TGcNEJbZlpuIIKu2eLi91Ldv-Hgro",
+  authDomain: "siembra-db.firebaseapp.com",
+  projectId: "siembra-db",
+  storageBucket: "siembra-db.firebasestorage.app",
+  messagingSenderId: "571473982087",
+  appId: "1:571473982087:web:7bb27a0af5f3e20576fe22"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
